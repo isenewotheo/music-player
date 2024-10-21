3511f240caf3f113122235cfc22fdcb19403fa54
Oludare.Theophilus <oludare.theophilus@anyskillsinc.onmicrosoft.com>
2024-10-21 18:42:07 +0100

    Project setup

---
+++ start/routes.ts
@@ -0,0 +1,22 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+
+router.get('/', async () => {
+  return {
+    hello: 'world',
+  }
+})
+
+router.get("/about", async () => {
+  return {
+    "hello" : "world this is the about"
+  }
+})
